32728e50efec80097004c25ccc2d8aab0185bc29
Guilherme Ferreira <ferreiraga@outlook.com>
2022-03-31 18:09:31 -0300

    Initial Commit

---
+++ src/environments/environments.js
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+
+    SpotifyClientID: '236c7c685b2b49b3a9e37497e187e1b1',
+    SpotifyClientSecret: '8f052c1140aa4e38af577d2b5ff90f57',
+    SpotifyRedirectUri: 'https://guilheeeeeeerme.github.io/so_toca_app/callback/'
+
+}
